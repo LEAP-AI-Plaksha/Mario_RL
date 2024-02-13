6015b868994eabdf89f1ab21c5d22a895108c173
Pranjal Rastogi <25995849+PjrCodes@users.noreply.github.com>
2024-02-14 00:24:52 +0530

    Added replay function.

--- replay.py
+++ replay.py
@@ -1,14 +1,15 @@
 import random, datetime
 from pathlib import Path
+import gym
 
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 from gym.wrappers import FrameStack, GrayScaleObservation, TransformObservation
 from nes_py.wrappers import JoypadSpace
 
-from mario import MetricLogger, Mario, ResizeObservation, SkipFrame
+from mario_2 import Mario, MetricLogger, ResizeObservation, SkipFrame
 
-env = gym_super_mario_bros.make('SuperMarioBros-1-1-v0',  apply_api_compatibility=True, render_mode='rgb_array')
+env = gym_super_mario_bros.make('SuperMarioBros-v0',  apply_api_compatibility=True, render_mode='human')
 
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
 
@@ -16,14 +17,19 @@ env = SkipFrame(env, skip=4)
 env = GrayScaleObservation(env, keep_dim=False)
 env = ResizeObservation(env, shape=84)
 env = TransformObservation(env, f=lambda x: x / 255.)
-env = FrameStack(env, num_stack=4)
+
+if gym.__version__ < "0.26":
+    env = FrameStack(env, num_stack=4, new_step_api=True)
+else:
+    env = FrameStack(env, num_stack=4)
+
 
 env.reset()
 
 save_dir = Path('checkpoints') / datetime.datetime.now().strftime('%Y-%m-%dT%H-%M-%S')
 save_dir.mkdir(parents=True)
 
-checkpoint = Path('checkpoints/2024-02-13T10-36-24/mario_net_2.chkpt')
+checkpoint = Path('checkpoints/2024-02-13T10-38-49/mario_net_30.chkpt')
 mario = Mario(state_dim=(4, 84, 84), action_dim=env.action_space.n, save_dir=save_dir, checkpoint=checkpoint)
 mario.exploration_rate = mario.exploration_rate_min
 
@@ -39,10 +45,11 @@ for e in range(episodes):
 
         action = mario.act(state)
 
-        next_state, reward, done, info = env.step(action)
+        # obs, reward, done, trunk, info = env.step(action)
 
+        next_state, reward, done, trunc, info = env.step(action)
         mario.cache(state, next_state, action, reward, done)
-
+        # q, loss = mario.learn()
         logger.log_step(reward, None, None)
 
         state = next_state
